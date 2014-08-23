3f2256ad482dee433da545656bc13a3faad54fa9
Thiago van Dieten <nktakumi@gmail.com>
2014-08-23 20:22:11 +0300

    Initial Commit

---
+++ Blog Reader/TVDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVDAppDelegate.h
+//  Blog Reader
+//
+//  Created by Thiago van Dieten on 23/08/14.
+//  Copyright (c) 2014 Thiago van Dieten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blog Reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blog Reader
+//
+//  Created by Thiago van Dieten on 23/08/14.
+//  Copyright (c) 2014 Thiago van Dieten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TVDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TVDAppDelegate class]));
+    }
+}
